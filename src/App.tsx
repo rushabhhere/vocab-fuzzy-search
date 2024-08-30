faa3c5f071cec994405feda9f49fdeca3cf8f9ff
Rushabh Javeri <javeri.rushabh45@gmail.com>
2024-08-30 08:46:37 +0530

    added source code link

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,19 @@ function App() {
   };
 
   return (
-    <main className="flex flex-col items-center mt-10 w-full mb-10">
-      <h1 className="text-4xl font-semibold mb-10 mx-5">Vocab Fuzzy Search</h1>
+    <main className="flex flex-col items-center w-full mt-16 mb-10">
+      <div className="fixed top-3 right-5">
+        <a
+          className="text-sm text-blue-500 hover:underline"
+          href="https://github.com/rushabhhere/vocab-fuzzy-search"
+          target="_blank"
+        >
+          Source Code
+        </a>
+      </div>
+      <h1 className="mx-5 mb-10 text-4xl font-semibold text-center">
+        Vocab Fuzzy Search
+      </h1>
       <div className="w-[750px] max-w-full px-5">
         <input
           type="text"
@@ -35,14 +46,14 @@ function App() {
           autoFocus
           value={search}
           onChange={e => setSearch(e.target.value)}
-          className="py-2 px-4 w-full inline-block border border-gray-300 rounded-md mb-2"
+          className="inline-block w-full px-4 py-2 mb-2 border border-gray-300 rounded-md"
         />
         <div className="mb-8">
           <label htmlFor="limit" className="mr-3">
             Max Results:
           </label>
           <input
-            className="p-2 border border-gray-300 rounded-md inline-block"
+            className="inline-block p-2 border border-gray-300 rounded-md"
             type="number"
             id="limit"
             min="1"
@@ -51,7 +62,7 @@ function App() {
             onChange={handleLimitChange}
           />
         </div>
-        <section className="space-y-5 max-w-full">
+        <section className="max-w-full space-y-5">
           {results.map(result => (
             <Card key={result.item.key} data={result.item} />
           ))}
