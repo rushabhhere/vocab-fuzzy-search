ae6a3663c9d3192d672723ca7b7632b629648457
Rushabh Javeri <javeri.rushabh45@gmail.com>
2024-09-02 13:45:35 +0530

    improve NoRelevantResults message

--- src/components/NoRelevantResults.tsx
+++ src/components/NoRelevantResults.tsx
@@ -4,7 +4,7 @@ const NoRelevantResults: FC = () => {
   return (
     <div className="flex flex-col items-center gap-5 p-8 text-gray-600">
       <h4 className="text-2xl text-center">
-        Your search <i>yielded</i> no relevant results
+        Your search <i>yielded</i> no <i>apropos</i> results
       </h4>
       <img className="w-[300px]" src="/images/crickets.webp" alt="*crickets*" />
     </div>
